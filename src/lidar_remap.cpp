74c7efcdef4fe433b52a0f548e03675ec53da2f8
teopa <matteoromilio.pasqual@mail.polimi.it>
2024-04-18 12:50:07 +0200

    n3

--- src/lidar_remap.cpp
+++ src/lidar_remap.cpp
@@ -1,19 +1,18 @@
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
 
-void fixCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+class LidarRemap{
+    private:
+
+    public:
     
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "lidar_remap");
-    ros::NodeHandle nodeHandle;
 
-    ros::Publisher publ = nodeHandle.advertise<nav_msgs::Odometry>("/pointcloud_remapped", 1);
-    ros::Subscriber subscr = nodeHandle.subscribe("/os_cloud_node/points", 1, fixCallback);
-
-    ros::Rate loop_rate(1);
-
+    LidarRemap lidar_remap;
+    ros::spin()
 
     return 0;
 }
