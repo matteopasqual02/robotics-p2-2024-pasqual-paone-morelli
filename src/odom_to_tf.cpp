4d40f3723ce3edf42ccf63f497fd40fd3e1b129f
Arianna <ariannapaone02@gmail.com>
2024-04-19 09:48:53 +0200

    nodo2

--- src/odom_to_tf.cpp
+++ src/odom_to_tf.cpp
@@ -7,35 +7,37 @@
 class tf_sub_pub{
 public:
     tf_sub_pub(){
+        ros::NodeHandle nodeHandle;
+        nodeHandle.getParam("child_frame",child_frame); //mette il parametro child_frame in child_frame
+        nodeHandle.getParam("root_frame",root_frame);
         sub = nodeHandle.subscribe("input_odom",1,&tf_sub_pub::callback, this);
     }
 void callback(const nav_msgs::Odometry::ConstPtr& msg){
-        std::string root_frame;
-        std::string child_frame;
-        nodeHandle.getParam("child_frame",child_frame); //mette il parametro child_frame in child_frame
-        nodeHandle.getParam("root_frame",root_frame);
+        //std::string root_frame;
+        //std::string child_frame;
+
         tf::Transform transform;
         transform.setOrigin(tf::Vector3(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z));
-        /*tf::Quaternion q;
-        q.setRPY(0, 0, msg->theta);
-        transform.setRotation(q);*/
+        tf::Quaternion q;
+        tf::quaternionMsgToTF(msg->pose.pose.orientation, q);
+        transform.setRotation(q);
+        ROS_INFO("child_frame: %s", child_frame.c_str()); // Stampare il valore di child_frame
+        ROS_INFO("root_frame: %s", root_frame.c_str()); // Stampare il valore di root_frame
         br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), root_frame, child_frame));
 
 }
 private:
-    ros::NodeHandle nodeHandle;
+    //ros::NodeHandle nodeHandle;
     tf::TransformBroadcaster br;
     ros::Subscriber sub;
-    /*std::string root_frame;
+    std::string root_frame;
     std::string child_frame;
-    nodeHandle.getParam("child_frame",child_frame); //mette il parametro child_frame in child_frame
+    /*nodeHandle.getParam("child_frame",child_frame); //mette il parametro child_frame in child_frame
     nodeHandle.getParam("root_frame",root_frame);*/
 };
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "odom_to_tf");
     tf_sub_pub my_tf_sub_pub;
-
     ros::spin();
-    return 0;
-}
+    return 0;}
