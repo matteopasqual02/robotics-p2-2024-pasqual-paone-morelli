bde172ca5b7d7eb6944670dd8ef8af564b85752e
teopa <matteoromilio.pasqual@mail.polimi.it>
2024-04-22 17:19:06 +0200

    node 3 fixed

--- src/lidar_remap.cpp
+++ src/lidar_remap.cpp
@@ -2,33 +2,38 @@
 #include "sensor_msgs/PointCloud2.h"
 #include <dynamic_reconfigure/server.h>
 #include <first_project/parametersConfig.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
 
-class LidarRemap{
+class LidarRemap {
 private:
     ros::NodeHandle nodeHandle;
-    ros::Publisher publ;
-    ros::Subscriber subscr; 
+    ros::Publisher pub;
+    ros::Subscriber sub;
     dynamic_reconfigure::Server<first_project::parametersConfig> server;
-    std::string frame_id;     
+    std::string frame_id;
+    tf::TransformBroadcaster broadcaster;
+    tf::TransformListener listener;
+    tf::StampedTransform stamped;
 
 public:
     LidarRemap() {
         server.setCallback(boost::bind(&LidarRemap::reconfigCallback, this, _1, _2));
-        subscr = nodeHandle.subscribe("/os_cloud_node/points", 1, &LidarRemap::pointCloudCallback, this);
-        publ = nodeHandle.advertise<sensor_msgs::PointCloud2>("/pointcloud_remapped",1);
+        sub = nodeHandle.subscribe("/os_cloud_node/points", 1, &LidarRemap::pointCloudCallback, this);
+        pub = nodeHandle.advertise<sensor_msgs::PointCloud2>("/pointcloud_remapped", 1);
     }
 
     void reconfigCallback(first_project::parametersConfig &config, uint32_t level) {
         frame_id = config.topic_choice;
-        ROS_INFO("fame_id: %s",frame_id.c_str());
+        ROS_INFO("Frame ID: %s", frame_id.c_str());
     }
 
     void pointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
+        
+        listener.lookupTransform(frame_id, msg->header.frame_id, ros::Time(0), stamped);
 
-        sensor_msgs::PointCloud2 modified_msg = *msg;
-        modified_msg.header.frame_id = frame_id;
-
-        publ.publish(modified_msg);
+        broadcaster.sendTransform(tf::StampedTransform(stamped, ros::Time::now(), "/pointcloud_remapped", msg->header.frame_id));
+        
     }
 };
 
@@ -40,3 +45,5 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+
+
